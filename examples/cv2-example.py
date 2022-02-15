3b116496269744fe27a77eec3b03480eda5b7470
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2022-02-15 16:41:05 +0100

    Reorder variables

--- examples/cv2-example.py
+++ examples/cv2-example.py
@@ -14,8 +14,8 @@ cv2.imshow("Image", image_array)
 cv2.waitKey(0)
 
 # Resize image to half it's size
-x, y, *_ = image_array.shape
-resized_image = cv2.resize(image_array, (int(y / 2), int(x / 2)))
+y, x, *_ = image_array.shape
+resized_image = cv2.resize(image_array, (int(x / 2), int(y / 2)))
 
 # Write image to disk
 cv2.imwrite("ny_katt.jpg", resized_image)

df94ceea7195eca93ab7b86e64412fef313aabdd
julie <julielbr@stud.ntnu.no>
2022-03-22 18:43:22 +0100

    added function to convert RGB to Yuv

---
+++ src/processing/dataprocessing.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+# Use TF Ops to process.
+def process_input(input, input_size, upscale_factor):
+    input = tf.image.rgb_to_yuv(input)
+    last_dimension_axis = len(input.shape) - 1
+    y, u, v = tf.split(input, 3, axis=last_dimension_axis)
+    return tf.image.resize(y, [input_size, input_size], method="area")
+
+
+def process_target(input):
+    input = tf.image.rgb_to_yuv(input)
+    last_dimension_axis = len(input.shape) - 1
+    y, u, v = tf.split(input, 3, axis=last_dimension_axis)
+    return y
+

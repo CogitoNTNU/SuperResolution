396384038fe626371a70c7aef1d03f1014822552
Jon Rodtang <JonRodtang@users.noreply.github.com>
2022-04-05 18:27:32 +0200

    placed tf.nn.depth_to_space in Lambda layer

--- src/model/model.py
+++ src/model/model.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import Input
-from tensorflow.keras.layers import Conv2D, MaxPool2D
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Lambda
 
-def getModel(channels=3, upscale_factor=1):
+def getModel(channels=1, upscale_factor=2):
     conv_args = {
         "activation": "relu",
         "kernel_initializer": "Orthogonal",
@@ -13,13 +13,12 @@ def getModel(channels=3, upscale_factor=1):
         [
             Input(shape=(None,None, channels)),
             Conv2D(64, 5, **conv_args),
-            Conv2D(64, 3, **conv_args),
             Conv2D(channels * (upscale_factor ** 2), 3, **conv_args),
+            Conv2D(64, 3, **conv_args),
             MaxPool2D(),
             Conv2D(32, 3, **conv_args),
             Conv2D(channels * (upscale_factor ** 2), 3, **conv_args),
-            tf.nn.depth_to_space(upscale_factor),
+            Lambda(lambda x: tf.nn.depth_to_space(x, upscale_factor)),
         ]
     )
-#TODO add other layers than only conv2d
     return model

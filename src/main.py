9606416597ef86bf80210f1a26ea18b6768e1a57
espen <espenbfo@stud.ntnu.no>
2022-04-05 18:36:04 +0200

    Add training with checkpointing

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 from model.example_model import get_model
 from processing.generator import get_generators
-
-BATCH_SIZE = 32
-
+from model.modelCheckpoint import get_checkpoint
+from tensorflow.keras.models import load_model
+BATCH_SIZE = 128
+MODEL_SAVE_PATH = "model/checkpoint"
+CONTINUE_TRAINING = False
+UPSCALE_FACTOR = 2
+TARGET_SIZE = 128
 if __name__ == "__main__":
-    gen = get_generators("../data2", (64, 64), (128, 128), BATCH_SIZE)
-    model = get_model(1, 2)
+    checkpoint = get_checkpoint(MODEL_SAVE_PATH)
+    gen = get_generators("../data2", (TARGET_SIZE//UPSCALE_FACTOR, TARGET_SIZE//UPSCALE_FACTOR), (TARGET_SIZE, TARGET_SIZE), BATCH_SIZE)
+    if CONTINUE_TRAINING:
+        model = load_model(MODEL_SAVE_PATH)
+    else:
+        model = get_model(1, UPSCALE_FACTOR)
     model.compile(loss="mean_squared_error", optimizer="adam")
     print(model.summary())
-    model.fit_generator(gen, epochs=1, steps_per_epoch=1000)
+    model.fit_generator(gen, epochs=10, steps_per_epoch=int(4400/BATCH_SIZE), callbacks=[checkpoint])

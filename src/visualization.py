85a20c2a0feff223c0b8000ca6b7c1b998ba89c3
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2022-04-19 16:45:33 +0200

    Add simple test of model

---
+++ src/visualization.py
@@ -0,0 +1,36 @@
+from tensorflow.keras.models import load_model
+import cv2
+from processing.generator import get_generators
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_folder', type=str,
+                        help='data folder')
+    parser.add_argument('-b', "--batch_size", type=int, default=32,)
+    parser.add_argument('-u', "--upscale_factor", type=int, default=2,)
+    parser.add_argument('-m', "--model_save_path", type=str, default="model/checkpoint",)
+    parser.add_argument('-t', "--target_size", type=int, default=128,)
+    parser.add_argument('-e', "--epochs", type=int, default=10,)
+    parser.add_argument('-c', "--continue_training", action="store_true")
+    args = parser.parse_args()
+
+    model = load_model(args.model_save_path)
+
+    target_size = args.target_size
+    upscale_factor = args.upscale_factor
+    batch_size = args.batch_size
+    gen = get_generators(args.data_folder, (target_size//upscale_factor, target_size//upscale_factor), (target_size, target_size), batch_size)
+
+    images = next(gen)
+
+    predictions = model.predict(images)
+
+    print(images[0][0].shape)
+    for i, image in enumerate(images[0]):
+        cv2.imwrite(f"results/{i}_before.jpg", images[0][i]*255)
+        cv2.waitKey()
+        cv2.imwrite(f"results/{i}_after.jpg", predictions[i]*255)
+        cv2.waitKey()
+        cv2.imwrite(f"results/{i}_naive.jpg", cv2.resize(images[0][i], (target_size, target_size))*255)
+        cv2.waitKey()
